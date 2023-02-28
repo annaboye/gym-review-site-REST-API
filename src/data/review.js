a97b4c8b09217995994b7711f186363f6b7a9551
Angie <angelicareutersward@hotmail.com>
2023-02-28 13:16:53 +0100

    uppdat user.js samt påbörjad review.js mockdata

---
+++ src/data/review.js
@@ -0,0 +1,19 @@
+exports.review = [
+    {
+        title: "Great!",
+        description: "Lots of space, very clean machines, large area for free weights, and friendly staff! I'll be back!",
+        email: "ankanpankan@email.se",
+        password: "secret",
+        is_admin: TRUE,
+      },
+]
+
+// Table review {
+//     title TEXT
+//     description TEXT
+//     number_of_stars INTEGER
+//     fk_gym_id INTEGER [ref: > gym.id]
+//     fk_user_id INTEGER [ref: > user.id]
+  
+//    }
+  
