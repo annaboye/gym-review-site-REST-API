2173e3e982af351ff90cdb36d601c31ffa019c7e
Hamajaff <jaff226@gmail.comm>
2023-02-28 14:11:08 +0100

    minor fix

--- src/data/city.js
+++ src/data/city.js
@@ -1,14 +1,11 @@
 exports.city =[
     {
-        id: 1,
         city_name: "Stockholm"
     },
     {
-        id: 2,
         city_name: "Göterborg"
     },
     {
-        id: 3,
         city_name: "Malmö"
     }
 ]
